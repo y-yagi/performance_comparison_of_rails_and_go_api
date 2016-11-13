7d5413c7173acabeb5e51ff4b64f95287bce72f7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-11-13 09:35:07 +0900

    update request count

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 # ./go_api
 # RACK_ENV=production be puma -C puma.rb
 
-siege -r 100 -c 50 -b -q  http://localhost:3000/users > rails_api_result.log 2>&1
-siege -r 100 -c 50 -b -q  http://localhost:4000/users > go_api_result.log 2>&1
-siege -r 100 -c 50 -b -q  http://localhost:9292/users > roda_sequel_result.log 2>&1
+siege -r 200 -c 50 -b -q  http://localhost:3000/users > rails_api_result.log 2>&1
+siege -r 200 -c 50 -b -q  http://localhost:4000/users > go_api_result.log 2>&1
+siege -r 200 -c 50 -b -q  http://localhost:9292/users > roda_sequel_result.log 2>&1
